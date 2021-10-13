bea24eb4cb98c45d4e095a02d117b352a474c173
LEEHANDS <31511326+namunny@users.noreply.github.com>
2021-10-13 17:06:03 +0900

    Update battshield.py

--- battshield.py
+++ battshield.py
@@ -9,10 +9,10 @@ from time import sleep
 import smbus
 
 
-ld_100 = LED(26)
-ld_75 = LED(19)
-ld_50 = LED(13)
-ld_25 = LED(6)
+ld_100 = LED(6)
+ld_75 = LED(13)
+ld_50 = LED(19)
+ld_25 = LED(26)
 
 class battshield :
   def __init__(self):
@@ -24,27 +24,27 @@ class battshield :
     
   def indicatebatt(self,volt):
     if volt < 3000 :
-      ld_100.off()
-      ld_75.off()
-      ld_50.off()
-      ld_25.on()
-    elif volt < 3500 :
-      ld_100.off()
-      ld_75.off()
-      ld_50.on()
-      ld_25.on()
-      
-    elif volt < 4000 :
-      ld_100.off()
-      ld_75.on()
-      ld_50.on()
-      ld_25.on()
-      
-    else :
       ld_100.on()
       ld_75.on()
       ld_50.on()
-      ld_25.on()
+      ld_25.off()
+    elif volt < 3500 :
+      ld_100.on()
+      ld_75.on()
+      ld_50.off()
+      ld_25.off()
+      
+    elif volt < 4000 :
+      ld_100.on()
+      ld_75.off()
+      ld_50.off()
+      ld_25.off()
+      
+    else :
+      ld_100.off()
+      ld_75.off()
+      ld_50.off()
+      ld_25.off()
       
     
     
