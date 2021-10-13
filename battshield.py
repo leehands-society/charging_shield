3362101bb8bae45e805382470b3664e1f41fa013
LEEHANDS <31511326+namunny@users.noreply.github.com>
2021-10-13 16:12:11 +0900

    Update battshield.py
    mod battshield.py

--- battshield.py
+++ battshield.py
@@ -1,4 +1,16 @@
-
+from gpiozero import LED
+from time import sleep
+import smbus
 
 class battshield :
+  def __init__(self)
+    self.battlevel = 0
+    self.battstatus = 0
+    
+  def print_test(self):
+    print("battshield test ok")
+    
+  def indicatebatt(self,volt):
+    
+    
 
