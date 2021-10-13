23725641fb54e7fcf1f9cbad287f455b603bd30e
LEEHANDS <31511326+namunny@users.noreply.github.com>
2021-10-13 17:09:45 +0900

    Update battshield.py

--- battshield.py
+++ battshield.py
@@ -18,30 +18,39 @@ class battshield :
   def __init__(self):
     self.battlevel = 0
     self.battstatus = 0
+    self.flashingCnt = 0
     
   def print_test(self):
     print("battshield test ok")
     
   def indicatebatt(self,volt):
+    if self.flashingCnt  == 1 :
+      self.flashingCnt = 0
+    else
+      self.flashingCnt = 1
+    
     if volt < 3000 :
       ld_100.on()
       ld_75.on()
       ld_50.on()
-      ld_25.off()
-    elif volt < 3500 :
+      if self.flashingCnt = 1:      ld_25.off()
+      else :                        ld_25.on()
+    elif volt < 3700 :
       ld_100.on()
       ld_75.on()
-      ld_50.off()
-      ld_25.off()
-      
-    elif volt < 4000 :
+      if self.flashingCnt = 1:      ld_50.off()
+      else :                        ld_50.on()
+      ld_25.off()      
+    elif volt < 4200 :
       ld_100.on()
-      ld_75.off()
+      if self.flashingCnt = 1:      ld_75.off()
+      else :                        ld_75.on()
       ld_50.off()
       ld_25.off()
       
     else :
-      ld_100.off()
+      if self.flashingCnt = 1:      ld_100.off()
+      else :                        ld_100.on()
       ld_75.off()
       ld_50.off()
       ld_25.off()
