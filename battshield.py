1f5c5b8fc3638075cfd01c7bcef00f7c18dbcef1
LEEHANDS <31511326+namunny@users.noreply.github.com>
2021-10-14 18:58:35 +0900

    Update battshield.py

--- battshield.py
+++ battshield.py
@@ -36,23 +36,35 @@ class battshield :
       ld_100.on()
       ld_75.on()
       ld_50.on()
-      if self.flashingCnt == 1:      ld_25.off()
-      else :                         ld_25.on()
+      if current < 10 :
+        ld_25.off()
+      else:
+        if self.flashingCnt == 1:      ld_25.off()
+        else :                         ld_25.on()
     elif voltage < 3700 :
       ld_100.on()
       ld_75.on()
-      if self.flashingCnt == 1:      ld_50.off()
-      else :                        ld_50.on()
+      if current < 10:
+        ld_50.off()
+      else:
+        if self.flashingCnt == 1:      ld_50.off()
+        else :                        ld_50.on()
       ld_25.off()      
     elif voltage < 4200 :
       ld_100.on()
-      if self.flashingCnt == 1:      ld_75.off()
-      else :                        ld_75.on()
+      if current < 10:
+        ld_75.off()
+      else:
+        if self.flashingCnt == 1:      ld_75.off()
+        else :                        ld_75.on()
       ld_50.off()
       ld_25.off()
     else :
-      if self.flashingCnt == 1:      ld_100.off()
-      else :                        ld_100.on()
+      if current < 10 :
+        ld_100.off()
+      else:
+        if self.flashingCnt == 1:      ld_100.off()
+        else :                        ld_100.on()
       ld_75.off()
       ld_50.off()
       ld_25.off()
